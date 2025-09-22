29905f7ea2304671bc35b4f5ff6d9436a37507f9
DAIMON <st4809508@stud.duikt.edu.ua>
2025-09-22 15:28:32 +0300

    Add db

---
+++ SellCatcher.Api/Models/Comparison.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceComparisonApp.Shops
+{
+    public class Comparison
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; } = "";  // Название товара
+        public decimal Shop1Price { get; set; }  // Цена в магазине 1
+        public decimal Shop2Price { get; set; }  // Цена в магазине 2
+        public string CheaperIn { get; set; } = "";   // Где дешевле
+        public DateTime Date { get; set; }      // Когда сравнивали
+    }
+}
---
+++ SellCatcher.Api/Models/Shop2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceComparisonApp.Shops
+{
+    public class Shop2Product
+    {
+        public int Id { get; set; }  // Индекс товара
+        public string Name { get; set; } = "";  // Название товара
+        public decimal Price { get; set; }  // Цена товара
+    }
+}
