cc8d6eb2bae678efd5754a2a866652c2ecc0a02c
GARTIK\artur <arturiegorov07@gmail.com>
2025-10-20 17:46:35 +0300

    fix coment

--- SellCatcher.Api/Controllers/DiscountController.cs
+++ SellCatcher.Api/Controllers/DiscountController.cs
@@ -2,8 +2,6 @@ using Microsoft.AspNetCore.Mvc;
 using SellCatcher.Api.Models;
 using SellCatcher.Api.Services;
 
-// DiscountService не добавлял, потому-что Виталик сказал что не нужно. Но если нужно, могу добавить. 
-
 namespace SellCatcher.Api.Controllers
 {
     [ApiController]
