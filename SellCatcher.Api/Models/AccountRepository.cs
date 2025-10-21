cd5c76ea938113c16a6e8b21668c5754c391f5c0
K1F\k1f <rsdarX@gmail.com>
2025-10-21 22:35:01 +0300

    Account repo fix + test fix

--- SellCatcher.Api/Models/AccountRepository.cs
+++ SellCatcher.Api/Models/AccountRepository.cs
@@ -9,7 +9,17 @@ namespace SellCatcher.Api.Models
 {
     public class AccountRepository
     {
-        private readonly string _dbPath = @"C:\Users\Admin\Documents\sell-catcher-web\SellCatcher.Api\sellcatcher.db";
+        private readonly string _dbPath;
+
+        public AccountRepository()
+        {
+            var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            var DBPlace = Path.GetFullPath(Path.Combine(baseDirectory, @"..\..\..\"));
+
+            _dbPath = Path.Combine(DBPlace, "sellcatcher.db");
+        }
+
 
         public void Add(Account account)
         {
