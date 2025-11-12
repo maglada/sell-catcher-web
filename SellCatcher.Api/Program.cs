c5d222b11bff19b433fe62f6ccc13826fe3a233a
maglada <vitaliioperrenko@gmail.com>
2025-11-12 17:42:24 +0200

    parser on startup

--- SellCatcher.Api/Program.cs
+++ SellCatcher.Api/Program.cs
@@ -16,9 +16,12 @@ var jwtSecretKey = Environment.GetEnvironmentVariable("JWT_SECRET_KEY");
 var jwtLifetime = Environment.GetEnvironmentVariable("JWT_TOKEN_LIFETIME");
 var dbPath = Environment.GetEnvironmentVariable("DB_PATH");
 
+
+
 builder.Services.AddOpenApi();
 
 
+builder.Services.AddSingleton<ScraperFactory>();
 builder.Services.AddScoped<DiscountService>();
 builder.Services.AddScoped<AccountRepository>();
 builder.Services.AddControllers();
@@ -31,7 +34,32 @@ builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnC
 
 var app = builder.Build();
 
+var factory = app.Services.GetRequiredService<ScraperFactory>();
 
+var filepatterns = new List<string> { "NovusLinks_*.txt", "SilpoLinks_*.txt" };
+
+foreach (var pattern in filepatterns)
+{
+    var results = await factory.ProcessAllFilesAsync(
+        directory: Path.Combine(AppContext.BaseDirectory, "sites"),
+        filePattern: pattern
+    );
+
+    var productService = new ParserProductService.ProductService();
+    int totalSaved = 0;
+    int totalProducts = 0;
+
+    foreach (var result in results)
+    {
+        var products = result.Value;
+        totalProducts += products.Count;
+        int saved = productService.SaveProducts(products);
+        totalSaved += saved;
+    }
+
+    Console.WriteLine($"\nTotal products parsed: {totalProducts} from pattern {pattern}");
+    Console.WriteLine($"Saved to DB: {totalSaved}\n");
+}
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
