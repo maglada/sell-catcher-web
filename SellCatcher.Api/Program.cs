24261a33c138cd6c6bde6c71ff5015381fd0205c
VOLCODAV\maxim <maxim102.net@gmail.com>
2025-12-06 19:22:35 +0200

    upgradet some styles, little filter for products , Logo file (need to change width after changing image)

--- SellCatcher.Api/Program.cs
+++ SellCatcher.Api/Program.cs
@@ -42,33 +42,33 @@ builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnC
 
 var app = builder.Build();
 
-//var factory = app.Services.GetRequiredService<ScraperFactory>();
+var factory = app.Services.GetRequiredService<ScraperFactory>();
 
-//var filepatterns = new List<string> { "NovusLinks_*.txt", "SilpoLinks_*.txt" };
+var filepatterns = new List<string> { "NovusLinks_*.txt", "SilpoLinks_*.txt" };
 
-//foreach (var pattern in filepatterns)
-//{
-//    var results = await factory.ProcessAllFilesAsync(
-//        directory: Path.Combine(AppContext.BaseDirectory, "sites"),
-//        filePattern: pattern
-//    );
+foreach (var pattern in filepatterns)
+{
+  var results = await factory.ProcessAllFilesAsync(
+      directory: Path.Combine(AppContext.BaseDirectory, "sites"),
+      filePattern: pattern
+  );
 
-//    var productService = new ParserProductService.ProductService();
-//    int totalSaved = 0;
-//    int totalProducts = 0;
+  var productService = new ParserProductService.ProductService();
+  int totalSaved = 0;
+  int totalProducts = 0;
 
-//    foreach (var result in results)
-//    {
-//        string storeName = pattern.Contains("Novus") ? "Novus" : "Silpo";
-//        var products = result.Value;
-//        totalProducts += products.Count;
-//        int saved = productService.SaveProducts(products, storeName);
-//        totalSaved += saved;
-//    }
+  foreach (var result in results)
+  {
+    string storeName = pattern.Contains("Novus") ? "Novus" : "Silpo";
+    var products = result.Value;
+    totalProducts += products.Count;
+    int saved = productService.SaveProducts(products, storeName);
+    totalSaved += saved;
+  }
 
-//    Console.WriteLine($"\nTotal products parsed: {totalProducts} from pattern {pattern}");
-//    Console.WriteLine($"Saved to DB: {totalSaved}\n");
-//}
+  Console.WriteLine($"\nTotal products parsed: {totalProducts} from pattern {pattern}");
+  Console.WriteLine($"Saved to DB: {totalSaved}\n");
+}
 
 
 
